bb2683790444818168d2770ed401fc772569dee8
Mehul Arora <mehul@s2.dev>
2026-01-25 03:16:54 +0000

    Update S2 to 0.25.1

--- s2.rb
+++ s2.rb
@@ -1,24 +1,24 @@
 class S2 < Formula
   desc "CLI for S2"
   homepage "https://s2.dev"
-  version "0.25.0"
+  version "0.25.1"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/s2-streamstore/streamstore-cli/releases/download/#{version}/s2-x86_64-apple-darwin.zip"
-      sha256 "55f4ad1a5f62625e3f520b720d649a40863bbd95d3ed8723465db213d925d13e"
+      sha256 "3442feb9d943b59e2b5d30fd1451e4e5e942122da9d0fe1985ba56b0bee8dadf"
     else
       url "https://github.com/s2-streamstore/streamstore-cli/releases/download/#{version}/s2-aarch64-apple-darwin.zip"
-      sha256 "4f912599c76209cc7a94c34f633ef34fc0525b884a21bffbe0dce3692b58ffe6"
+      sha256 "9de5b942f1e9450fe8e0f7be786019a4cb366293704deb0df952e26e4d4f81ff"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/s2-streamstore/streamstore-cli/releases/download/#{version}/s2-x86_64-unknown-linux-gnu.zip"
-      sha256 "2e37e5b1162a73303436fb582c9f215828d1d7ef4d928467f6da2276039ccf8d"
+      sha256 "90adc922b63ce2f21f1dc2601938f411f5cac67124be8c08e40e8373f918d7bc"
     else
       url "https://github.com/s2-streamstore/streamstore-cli/releases/download/#{version}/s2-aarch64-unknown-linux-gnu.zip"
-      sha256 "5eaab3a73c140c40fcac5c44861d1cfc61f00aac97b03847561eba42a1c7c4b4"
+      sha256 "5e8ac9a91cf7a15639b57571df559328a11c9826281071dc7cc9147c29c309ae"
     end
   end
 
